9b9e1465c526e23224c4e5cc2379eeef2d9952ab
persistsolution-admin <persistsolution1@gmail.com>
2025-02-13 17:45:46 +0530

    update

--- src/config/knexconfig.ts
+++ src/config/knexconfig.ts
@@ -3,10 +3,10 @@ import { knex } from "knex";
 export const db = knex({
   client: "pg",
   connection: {
-    host: "database-3.crq6yqcualdr.ap-south-1.rds.amazonaws.com", // Update with your DB host
+    host: "database-5.crq6yqcualdr.ap-south-1.rds.amazonaws.com", // Update with your DB host
     user: "postgres", // Update with your DB username
-    password: "Rd9595454907", // Update with your DB password
-    database: "kwickbill_dev", // Update with your DB name
+    password: "pgrajat12345", // Update with your DB password
+    database: "kwickbill", // Update with your DB name
   },
   migrations: {
     directory: "./migrations", // Directory for migration files
