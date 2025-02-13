3ff2ccac68aa4c8a443e44ed10b46a5ebd5de4b6
persistsolution-admin <persistsolution1@gmail.com>
2025-02-13 17:56:33 +0530

    update

--- migrations/20250205095609_create_tbl_brands.ts
+++ migrations/20250205095609_create_tbl_brands.ts
@@ -1,12 +1,11 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('tbl_brands', function(table) {
         table.increments('id').primary();
         table.string('name', 255).nullable();
         table.integer('srno').notNullable();
-        table.boolean('status').notNullable().defaultTo(1);
+        table.specificType('status', 'smallint').notNullable().defaultTo(1);
         table.integer('createdby').notNullable();
         table.datetime('createddate').nullable();
         table.integer('modifiedby').notNullable();
@@ -14,8 +13,6 @@ export async function up(knex: Knex): Promise<void> {
     });
 }
 
-
 export async function down(knex: Knex): Promise<void> {
     return knex.schema.dropTable('tbl_brands');
 }
-
