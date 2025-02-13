1012c9e614c53a228c4d4884c1708b4dcae178f8
persistsolution <persistsolution1@gmail.com>
2025-02-13 18:14:36 +0530

    Update sequelize.ts

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("kwickbill_stage", "postgres", "pgrajat12345", {
+export const sequelize = new Sequelize("kwickbill_test", "postgres", "pgrajat12345", {
   host: "database-5.crq6yqcualdr.ap-south-1.rds.amazonaws.com",
   dialect: "postgres", // Or 'mysql' | 'mssql' | 'sqlite'
 });
