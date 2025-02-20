94d57aa004eec50672523b86b0948e55df626d24
persistsolution <persistsolution1@gmail.com>
2025-02-20 16:19:17 +0530

    Update knexconfig.ts

--- src/config/knexconfig.ts
+++ src/config/knexconfig.ts
@@ -1,13 +1,15 @@
 import { knex } from "knex";
+import 'dotenv/config';
 
 export const db = knex({
   client: "pg",
   connection: {
-    host: "database-5.crq6yqcualdr.ap-south-1.rds.amazonaws.com", // Update with your DB host
-    user: "postgres", // Update with your DB username
-    password: "pgrajat12345", // Update with your DB password
-    database: "kwickbill_test", // Update with your DB name
+    host: process.env.DB_HOST, // Update with your DB host
+    user: process.env.DB_USER, // Update with your DB username
+    password: process.env.DB_PASSWORD, // Update with your DB password
+    database: process.env.DB_NAME, // Update with your DB name
   },
+  useNullAsDefault: true,
   migrations: {
     directory: "./migrations", // Directory for migration files
   },
